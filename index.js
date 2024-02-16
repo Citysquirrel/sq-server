aaa0c0a4d93a486f3a9d9a8cf998928704f848ff
Citysquirrel <tok1324@naver.com>
2024-02-16 11:34:02 +0900

    치지직 방송 관련 알림 업데이트

--- index.js
+++ index.js
@@ -46,10 +46,10 @@ const discordPublicKey = process.env.DISCORD_PUBLIC_KEY;
 const discordToken = process.env.DISCORD_TOKEN;
 
 const twitchUrlPrefix = "https://www.twitch.tv/";
-const chzzkUrlPrefix = "https://chzzk.naver.com/live/";
-// 치지직 라이브 알림용 api: https://api.chzzk.naver.com/service/v2/channels/{channel_id}/live-detail
+const chzzkLivePrefix = "https://chzzk.naver.com/live/";
+const chzzkLiveAPI = (channelId) => `https://api.chzzk.naver.com/service/v2/channels/${channelId}/live-detail`;
 
-const clubId = "29424353";
+const naverCafeClubId = "29424353";
 
 const STREAMERS = {
 	notice: {
@@ -64,6 +64,15 @@ const STREAMERS = {
 		discordChannelId: "1179630895997669436",
 		color: 0x9adaff,
 		menuId: "152",
+		chzzk: {
+			channelId: "a6c4ddb09cdb160478996007bff35296",
+			channelName: undefined,
+			channelImageUrl: undefined,
+			status: undefined,
+			liveTitle: undefined,
+			openData: undefined,
+			closeData: undefined,
+		},
 	},
 	kanna: {
 		name: "airikanna_stellive",
@@ -74,6 +83,15 @@ const STREAMERS = {
 		discordChannelId: "1179630864217407489",
 		color: 0x373584,
 		menuId: "131",
+		chzzk: {
+			channelId: "f722959d1b8e651bd56209b343932c01",
+			channelName: undefined,
+			channelImageUrl: undefined,
+			status: undefined,
+			liveTitle: undefined,
+			openData: undefined,
+			closeData: undefined,
+		},
 	},
 };
 
@@ -107,7 +125,7 @@ const twitchTimer = setInterval(() => {
 }, 2000);
 
 const chzzkTimer = setInterval(() => {
-	getStream("tabi");
+	getChzzkStream("tabi");
 }, 2000);
 
 //! Functions
@@ -148,11 +166,130 @@ function getStream(streamer) {
 	}
 }
 
-function getChzzkLive(streamer) {
+function getChzzkStream(streamer) {
 	try {
+		const api = chzzkLiveAPI(STREAMERS[streamer].chzzk.channelId);
+		axios.get(api).then((res) => {
+			handleChzzkData(res.data, streamer);
+		});
 	} catch (err) {
 		console.log(err);
 		postErrorMessage(err);
+	}
+}
+
+async function handleChzzkData(data, streamer) {
+	console.log(data);
+	const { channel, status, liveTitle, openData, closeData } = data;
+	const { channelName, channelImageUrl } = channel;
+	if (STREAMERS[streamer].chzzk.status === undefined) {
+		//? 서버 실행 후 최초 실행
+		STREAMERS[streamer].chzzk.channelName = channelName;
+		STREAMERS[streamer].chzzk.channelImageUrl = channelImageUrl;
+		STREAMERS[streamer].chzzk.status = status;
+		STREAMERS[streamer].chzzk.liveTitle = liveTitle;
+		STREAMERS[streamer].chzzk.openData = openData;
+		STREAMERS[streamer].chzzk.closeData = closeData;
+	} else {
+		//? 이후 라이브 집계
+		if (status !== STREAMERS[streamer].chzzk.status) {
+			//? 라이브 상태 변화 감지
+			STREAMERS[streamer].chzzk.channelName = channelName;
+			STREAMERS[streamer].chzzk.channelImageUrl = channelImageUrl;
+			STREAMERS[streamer].chzzk.status = status;
+			STREAMERS[streamer].chzzk.liveTitle = liveTitle;
+			STREAMERS[streamer].chzzk.openData = openData;
+			STREAMERS[streamer].chzzk.closeData = closeData;
+			if (status === "OPEN") {
+				//!: 방송 ON 알림
+				postMessage(
+					streamer,
+					{
+						embeds: [
+							{
+								type: "rich",
+								author: {
+									name: STREAMERS[streamer].chzzk.channelName,
+									url: `${chzzkLivePrefix}${STREAMERS[streamer].chzzk.channelId}`,
+									icon_url: STREAMERS[streamer].chzzk.channelImageUrl,
+								},
+								title: `${STREAMERS[streamer].chzzk.channelName} 방송 ON!`,
+								url: `${chzzkLivePrefix}${STREAMERS[streamer].chzzk.channelId}`,
+								description: liveTitle,
+								footer: {
+									text: "Chzzk",
+									icon_url: "https://ssl.pstatic.net/static/nng/glive/icon/favicon.png",
+								},
+								thumbnail: { url: STREAMERS[streamer].chzzk.channelImageUrl },
+								timestamp: openData,
+								color: 0x6441a5,
+							},
+						],
+					},
+					`${STREAMERS[streamer].chzzk.channelName} 방송 ON 메시지 전송 성공!`
+				);
+			} else if (status === "CLOSE") {
+				//! 방송 OFF 알림
+				postMessage(
+					streamer,
+					{
+						embeds: [
+							{
+								type: "rich",
+								author: {
+									name: STREAMERS[streamer].chzzk.channelName,
+									url: `${chzzkLivePrefix}${STREAMERS[streamer].chzzk.channelId}`,
+									icon_url: STREAMERS[streamer].chzzk.channelImageUrl,
+								},
+								title: `${STREAMERS[streamer].chzzk.channelName} 방송 OFF`,
+								url: `${chzzkLivePrefix}${STREAMERS[streamer].chzzk.channelId}`,
+								description: liveTitle,
+								footer: {
+									text: "Chzzk",
+									icon_url: "https://ssl.pstatic.net/static/nng/glive/icon/favicon.png",
+								},
+								thumbnail: { url: STREAMERS[streamer].chzzk.channelImageUrl },
+								timestamp: closeData,
+								color: 0x6441a5,
+							},
+						],
+					},
+					`${STREAMERS[streamer].chzzk.channelName} 방송 OFF 메시지 전송 성공!`
+				);
+			}
+		} else {
+			//? 라이브 상태 변화 없을 시
+			if (liveTitle !== STREAMERS[streamer].chzzk.liveTitle) {
+				STREAMERS[streamer].chzzk.liveTitle = liveTitle;
+				//! title 변경 알림
+				postMessage(
+					streamer,
+					{
+						embeds: [
+							{
+								type: "rich",
+								author: {
+									name: STREAMERS[streamer].chzzk.channelName,
+									url: `${chzzkLivePrefix}${STREAMERS[streamer].chzzk.channelId}`,
+									icon_url: STREAMERS[streamer].chzzk.channelImageUrl,
+								},
+								title: `${STREAMERS[streamer].chzzk.channelName} 방송 제목 변경`,
+								url: `${chzzkLivePrefix}${STREAMERS[streamer].chzzk.channelId}`,
+								description: liveTitle,
+								footer: {
+									text: "Chzzk",
+									icon_url: "https://ssl.pstatic.net/static/nng/glive/icon/favicon.png",
+								},
+								thumbnail: { url: STREAMERS[streamer].chzzk.channelImageUrl },
+								timestamp: new Date(),
+								color: 0x6441a5,
+							},
+						],
+					},
+					`${STREAMERS[streamer].chzzk.channelName} 방송 ON 메시지 전송 성공!`
+				);
+			}
+		}
 	}
 }
 
@@ -236,7 +373,7 @@ function postErrorMessage(err) {
 }
 
 async function getNaverCafeData(streamer) {
-	const cafeNoticeUrl = `https://cafe.naver.com/ArticleList.nhn?search.clubid=${clubId}&search.menuid=${STREAMERS[streamer].menuId}`;
+	const cafeNoticeUrl = `https://cafe.naver.com/ArticleList.nhn?search.clubid=${naverCafeClubId}&search.menuid=${STREAMERS[streamer].menuId}`;
 	const res = await axios(cafeNoticeUrl, { responseType: "arraybuffer" });
 	const decoded = iconv.decode(res.data, "EUC-KR").toString();
 	const $ = cheerio.load(decoded);
